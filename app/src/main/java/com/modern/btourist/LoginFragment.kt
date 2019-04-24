8189eea6d4e85efb8a2a84b8d78812a9472fd998
Andrei Seceleanu <andreiseceleanu@Andreis-MacBook-Pro.local>
2019-04-25 00:55:12 +0300

    Initial commit Login Fragment Completed Register fragment and MainActivity created Imported all the needed components from Android Jetpack Imported fonts, images needed App launcher icon done Started using navigation with binding

---
+++ app/src/main/java/com/modern/btourist/LoginFragment.kt
@@ -0,0 +1,35 @@
+package com.modern.btourist
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.modern.btourist.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater, R.layout.fragment_login, container, false)
+
+        val registerText = binding.registerText
+        val loginButton = binding.loginButton
+
+        registerText.setOnClickListener(View.OnClickListener { view!!.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment()) })
+        loginButton.setOnClickListener(View.OnClickListener { view!!.findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToMainActivity2()) })
+
+        return binding.root
+
+    }
+
+
+}
